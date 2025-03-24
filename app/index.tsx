73f7250d0f2ab259c31b49c050d37d25078476f7
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2025-03-24 13:30:00 +0530

    ON IT

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
 import { Redirect } from "expo-router";
-import{Text} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuth } from "@clerk/clerk-expo";
 
 const Home = ()=>{
